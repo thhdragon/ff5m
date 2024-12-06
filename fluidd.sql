32595ea6d7911cecaa46e982f3809fb439115338
Sergei Rozhkov <fgup@zserg.ru>
2024-12-06 15:21:51 +0500

    START_ZMOD

--- fluidd.sql
+++ fluidd.sql
@@ -104,6 +104,14 @@ INSERT INTO "main"."namespace_store" ("namespace", "key", "value") VALUES ('flui
             "visible": true
         },
         {
+            "alias": "",
+            "categoryId": "0077449b-cd10-4059-aebd-bf17be6cb270",
+            "color": "",
+            "disabledWhilePrinting": false,
+            "name": "start_zmod",
+            "visible": true
+        },
+        {
             "alias": "Текущее время",
             "categoryId": "0077449b-cd10-4059-aebd-bf17be6cb270",
             "color": "",
