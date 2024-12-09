6fb88ef779225ee3ad22f1670a322fa90032b53e
Sergei Rozhkov <fgup@zserg.ru>
2024-12-09 14:40:44 +0500

    0.2.0

--- fluidd.sql
+++ fluidd.sql
@@ -64,6 +64,14 @@ INSERT INTO "main"."namespace_store" ("namespace", "key", "value") VALUES ('flui
             "visible": true
         },
         {
+            "alias": "Параметры ZMOD",
+            "categoryId": "0077449b-cd10-4059-aebd-bf17be6cb270",
+            "color": "",
+            "disabledWhilePrinting": false,
+            "name": "save_zmod_data",
+            "visible": true
+        },
+        {
             "alias": "Включить ZSSH",
             "categoryId": "0077449b-cd10-4059-aebd-bf17be6cb270",
             "color": "",
@@ -197,6 +205,22 @@ INSERT INTO "main"."namespace_store" ("namespace", "key", "value") VALUES ('flui
             "color": "",
             "disabledWhilePrinting": false,
             "name": "reboot",
+            "visible": true
+        },
+        {
+            "alias": "",
+            "categoryId": "944c031b-feef-4b75-badf-21c30508fb24",
+            "color": "",
+            "disabledWhilePrinting": false,
+            "name": "new_save_config",
+            "visible": true
+        },
+        {
+            "alias": "Закрыть диалоги",
+            "categoryId": "944c031b-feef-4b75-badf-21c30508fb24",
+            "color": "",
+            "disabledWhilePrinting": false,
+            "name": "close_dialogs",
             "visible": true
         },
         {
