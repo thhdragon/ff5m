076212af4fdc0e395610ef4413b85b8462a343d5
Sergei Rozhkov <fgup@zserg.ru>
2024-12-26 09:13:29 +0500

    zshaper

--- .shell/root/zshaper.sh
+++ .shell/root/zshaper.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
 echo "Подготовка изображения оси X. Ждите"
-sed -i 's/psd_x/psd_Y/' /opt/config/mod_data/calibration_data_x*.csv
-sed -i 's/psd_y/psd_x/' /opt/config/mod_data/calibration_data_x*.csv
-sed -i 's/psd_Y/psd_y/' /opt/config/mod_data/calibration_data_x*.csv
-python3 /opt/config/mod/.shell/root/zshaper/calibrate_shaper.py /opt/config/mod_data/calibration_data_x*.csv -o /opt/config/mod_data/calibration_data_x.png
+sed 's/psd_x/psd_Y/' /opt/config/mod_data/calibration_data_x_*.csv | sed 's/psd_y/psd_x/' | sed 's/psd_Y/psd_y/' | awk -F ',' '{print $1","$3","$2","$4","$5","$6","$7","$8","$9","$10}' >/opt/config/mod_data/calibration_data_xfix.csv
+python3 /opt/config/mod/.shell/root/zshaper/calibrate_shaper.py /opt/config/mod_data/calibration_data_xfix.csv -o /opt/config/mod_data/calibration_data_x.png
 echo "Подготовка изображения оси Y. Ждите"
-sed -i 's/psd_x/psd_Y/' /opt/config/mod_data/calibration_data_y*.csv
-sed -i 's/psd_y/psd_x/' /opt/config/mod_data/calibration_data_y*.csv
-sed -i 's/psd_Y/psd_y/' /opt/config/mod_data/calibration_data_y*.csv
-python3 /opt/config/mod/.shell/root/zshaper/calibrate_shaper.py /opt/config/mod_data/calibration_data_y*.csv -o /opt/config/mod_data/calibration_data_y.png
+sed 's/psd_x/psd_Y/' /opt/config/mod_data/calibration_data_y_*.csv | sed 's/psd_y/psd_x/' | sed 's/psd_Y/psd_y/' | awk -F ',' '{print $1","$3","$2","$4","$5","$6","$7","$8","$9","$10}' >/opt/config/mod_data/calibration_data_yfix.csv
+python3 /opt/config/mod/.shell/root/zshaper/calibrate_shaper.py /opt/config/mod_data/calibration_data_yfix.csv -o /opt/config/mod_data/calibration_data_y.png
