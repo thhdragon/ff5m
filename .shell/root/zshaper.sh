3288d1b4f7144ff2719e2daf83e9b1cc0ee5e1da
Sergei Rozhkov <fgup@zserg.ru>
2024-12-26 09:02:24 +0500

    zshaper

--- .shell/root/zshaper.sh
+++ .shell/root/zshaper.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 echo "Подготовка изображения оси X. Ждите"
+sed -i 's/psd_x/psd_Y/' /opt/config/mod_data/calibration_data_x*.csv
+sed -i 's/psd_y/psd_x/' /opt/config/mod_data/calibration_data_x*.csv
+sed -i 's/psd_Y/psd_y/' /opt/config/mod_data/calibration_data_x*.csv
 python3 /opt/config/mod/.shell/root/zshaper/calibrate_shaper.py /opt/config/mod_data/calibration_data_x*.csv -o /opt/config/mod_data/calibration_data_x.png
 echo "Подготовка изображения оси Y. Ждите"
+sed -i 's/psd_x/psd_Y/' /opt/config/mod_data/calibration_data_y*.csv
+sed -i 's/psd_y/psd_x/' /opt/config/mod_data/calibration_data_y*.csv
+sed -i 's/psd_Y/psd_y/' /opt/config/mod_data/calibration_data_y*.csv
 python3 /opt/config/mod/.shell/root/zshaper/calibrate_shaper.py /opt/config/mod_data/calibration_data_y*.csv -o /opt/config/mod_data/calibration_data_y.png
