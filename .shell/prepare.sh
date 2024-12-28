46acace656774e312de179d044d65d643a932a61
Sergei Rozhkov <fgup@zserg.ru>
2024-12-28 21:33:18 +0500

    fix_config

--- .shell/prepare.sh
+++ .shell/prepare.sh
@@ -2,6 +2,76 @@
 
 set -x
 
+fix_config()
+{
+    # Rem стукач
+    grep -q qvs.qiniuapi.com /etc/hosts || sed -i '2 i\127.0.0.1 qvs.qiniuapi.com' /etc/hosts
+
+    grep -q 'include check_md5.cfg'   /opt/config/printer.cfg && sed -i '/include check_md5.cfg/d'    /opt/config/printer.cfg
+
+    sed -i 's|\[include ./mod/display_off.cfg\]|\[include ./mod/mod.cfg\]|' /opt/config/printer.cfg
+
+    ! grep -q 'include ./mod/mod.cfg' /opt/config/printer.cfg && sed -i '2 i\[include ./mod/mod.cfg]' /opt/config/printer.cfg
+
+    grep -q 'include mod.user.cfg' /opt/config/printer.cfg && sed -i 's|\[include mod.user.cfg\]|\[include ./mod_data/user.cfg\]|' /opt/config/printer.cfg
+
+    ! grep -q 'include ./mod_data/user.cfg'  /opt/config/printer.cfg && sed -i '3 i\[include ./mod_data/user.cfg]'  /opt/config/printer.cfg
+    if ! grep -q '\[heater_bed' /opt/config/printer.cfg
+        then
+            cd /opt/config/
+
+            # Copy and remove from printer.base.cfg
+            if grep -q '\[heater_bed' /opt/config/printer.base.cfg
+                then
+                    sed -e '/^\[heater_bed/,/^\[/d' printer.base.cfg >printer.base.tmp
+                    diff -u printer.base.cfg printer.base.tmp | grep -v "printer.base.cfg" |grep "^-" | cut -b 2- >heater_bed.txt
+                    sed -i '$d' heater_bed.txt
+                    num=$(wc -l heater_bed.txt|cut  -d " " -f1)
+                    num=$(($num-1))
+                    sed -e "/^\[heater_bed/,+${num}d;" printer.base.cfg >printer.base.tmp
+                    mv printer.base.tmp printer.base.cfg
+                else
+                    echo "[heater_bed]
+heater_pin: PB9
+sensor_type: Generic 3950
+sensor_pin: PC3
+pullup_resistor: 4700
+control: pid
+pid_Kp: 32.79
+pid_Ki: 4.970
+pid_Kd: 54.118
+#control: watermark
+#max_power: 1.0
+min_temp: -100
+max_temp: 130
+
+" >heater_bed.txt
+            fi
+
+            num=$(cat -n printer.cfg |grep ./mod_data/user.cfg| awk '{print $1}')
+            head -n $num printer.cfg >printer.tmp
+            echo "" >>printer.tmp
+            cat heater_bed.txt >>printer.tmp
+            num=$(($num+1))
+            tail -n +$num printer.cfg >>printer.tmp
+            mv printer.tmp printer.cfg
+            rm heater_bed.txt || echo "Not heater_bed.txt"
+    fi
+
+    if grep -q '\[heater_bed' /opt/config/printer.base.cfg
+        then
+            cd /opt/config/
+            sed -e '/^\[heater_bed/,/^\[/d' printer.base.cfg >printer.base.tmp
+            diff -u printer.base.cfg printer.base.tmp | grep -v "printer.base.cfg" |grep "^-" | cut -b 2- >heater_bed.txt
+            sed -i '$d' heater_bed.txt
+            num=$(wc -l heater_bed.txt|cut  -d " " -f1)
+            num=$(($num-1))
+            sed -e "/^\[heater_bed/,+${num}d;" printer.base.cfg >printer.base.tmp
+            mv printer.base.tmp printer.base.cfg
+            rm -f heater_bed.txt
+    fi
+}
+
 start_prepare()
 {
     MOD=/data/.mod/.zmod
@@ -80,6 +150,7 @@ start_prepare()
       exit
     fi
 
+    fix_config
     echo "System start" >/data/logFiles/ssh.log
     mount -t proc /proc $MOD/proc
     mount --rbind /sys $MOD/sys
