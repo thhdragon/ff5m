93d57593d6d19ec124edd3f2ddb99de3f7268b11
Sergei Rozhkov <fgup@zserg.ru>
2024-12-29 19:58:29 +0500

    push

--- addMD5.sh
+++ addMD5.sh
@@ -6,7 +6,7 @@ if [ $# -ne 1 ]; then echo "Usage: ./$0 your_file.gcode"; exit 1; fi
 if ! [ -f $1 ]; then echo "File $1 not found"; exit 1; fi
 
 # Calculate the MD5 hash of the file
-md5=$(md5summ "$1" | awk '{print $1}')
+md5=$(md5sum "$1" | awk '{print $1}')
 
 # Define prefix
 prefix="; MD5:"
