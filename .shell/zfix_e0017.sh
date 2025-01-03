470a9d32b7583d82e759772e06b0bcf6ca70bfcc
Sergei Rozhkov <fgup@zserg.ru>
2025-01-03 23:59:22 +0500

    e0017

--- .shell/zfix_e0017.sh
+++ .shell/zfix_e0017.sh
@@ -4,13 +4,13 @@ F="/opt/klipper/klippy/toolhead.py"
 
 clear_klipper()
 {
-    find /opt/klipper/ -name __pycache__ -type d -exec rm -r "{}" \;
+    find /opt/klipper/ -name __pycache__ -type d -exec rm -r "{}" \; 2>/dev/null
     sync
-    find /opt/klipper/ -name *.pyc -exec rm "{}" \;
+    find /opt/klipper/ -name *.pyc -exec rm "{}" \; 2>/dev/null
     sync
     echo "Klipper был изменен. Сейчас будет перезагрузка"
     sleep 5
-#    reboot
+    reboot
 }
 
 if [ "$1" == "0" ]
