38efb35dcae1c542974767c24ff8105cf9559a2f
Sergei Rozhkov <fgup@zserg.ru>
2025-01-03 23:48:12 +0500

    e0017

--- .shell/zfix_e0017.sh
+++ .shell/zfix_e0017.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 F="/opt/klipper/klippy/toolhead.py"
+
 clear_klipper()
 {
     find /opt/klipper/ -name __pycache__ -type d -exec rm -r "{}" \;
@@ -12,7 +13,7 @@ clear_klipper()
 #    reboot
 }
 
-if [ "$1" == 0 ]
+if [ "$1" == "0" ]
     then
         grep -q "LOOKAHEAD_FLUSH_TIME = 0.5" $F && exit 0
         sed -i 's|^LOOKAHEAD_FLUSH_TIME.*|LOOKAHEAD_FLUSH_TIME = 0.250|' $F
