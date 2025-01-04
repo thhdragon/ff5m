a5e264fa265d5b047cbe0522118a9d1c58417bc7
Sergei Rozhkov <fgup@zserg.ru>
2025-01-04 23:52:02 +0500

    Test Alexander

--- .shell/zfs.sh
+++ .shell/zfs.sh
@@ -1,22 +1,27 @@
 #!/bin/sh
 
-if [ "$2" == "0" ];
-    then
-        echo "В фоне будет записано $1 MB"
-        dd if=/dev/zero of=/data/test.img bs=1M count=$1 conv=fsync 2>/dev/null &
-    else
-        echo "Идет тестирование записи/чтения $1 MB данных. Ждите..."
-        START=$(date +%s)
-        dd if=/dev/zero of=/data/test.img bs=1M count=$1 conv=fsync 2>/dev/null
-        END=$(date +%s)
-        TIME=$(($END-$START))
-        SPEED_W=$(($1/$TIME))
+# Более точный замер времени - Alexander
 
-        START=$(date +%s)
-        dd if=/data/test.img of=/dev/null bs=1M count=$1 conv=fsync 2>/dev/null
-        END=$(date +%s)
-        TIME=$(($END-START))
-        SPEED_R=$(($1/$TIME))
-        rm -f /data/test.img
-        echo "Записано $1 MB. Запись ${SPEED_W} MB/s. Чтение ${SPEED_R} MB/s."
+SIZE=$1
+FILE="/data/test.img"
+if [ "$2" == "0" ]
+    then
+        echo "В фоне будет записано ${SIZE} MB"
+        dd if=/dev/zero of=$FILE bs=1M count=${SIZE} conv=fsync 2>/dev/null &
+    else
+        echo "Идет тестирование записи/чтения ${SIZE} MB данных. Ждите..."
+
+        read up rest </proc/uptime; t1="${up%.*}${up#*.}"
+        dd if=/dev/zero of=$FILE bs=1M count=${SIZE} conv=fsync 2>/dev/null
+        read up rest </proc/uptime; t2="${up%.*}${up#*.}"
+        TIME_W=$(( 10*(t2-t1) ))
+
+        read up rest </proc/uptime; t1="${up%.*}${up#*.}"
+        dd if=$FILE of=/dev/null bs=1M count=${SIZE} conv=fsync 2>/dev/null
+        read up rest </proc/uptime; t2="${up%.*}${up#*.}"
+        TIME_R=$(( 10*(t2-t1) ))
+
+        rm -f $FILE
+
+        awk "BEGIN {print \"Записано $SIZE MB. Запись: \" ($SIZE * 1000 / $TIME_W) \" MB/s. Чтение: \" ($SIZE * 1000 / $TIME_R) \" MB/s.\"}"
 fi
