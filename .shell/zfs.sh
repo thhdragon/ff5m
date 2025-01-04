da9d558b47ead9c89d1a7b301afcf61b20766d2c
Sergei Rozhkov <fgup@zserg.ru>
2025-01-04 20:38:10 +0500

    fix

--- .shell/zfs.sh
+++ .shell/zfs.sh
@@ -3,18 +3,18 @@
 if [ "$2" == "0" ];
     then
         echo "В фоне будет записано $1 MB"
-        dd if=/dev/urandom of=/data/test.img bs=1M count=$1 conv=fsync &
+        dd if=/dev/urandom of=/data/test.img bs=1M count=$1 conv=fsync 2>/dev/null &
     else
         START=$(date +%s)
-        dd if=/dev/urandom of=/data/test.img bs=1M count=$1 conv=fsync
+        dd if=/dev/urandom of=/data/test.img bs=1M count=$1 conv=fsync 2>/dev/null
         END=$(date +%s)
-        TIME=$(($START-$END))
+        TIME=$(($END-$START))
         SPEED_W=$(($1/$TIME))
 
         START=$(date +%s)
-        dd if=/data/test.img of=/dev/zero bs=1M count=$1 conv=fsync
+        dd if=/data/test.img of=/dev/null bs=1M count=$1 conv=fsync 2>/dev/null
         END=$(date +%s)
-        TIME=$(($START-$END))
+        TIME=$(($END-START))
         SPEED_R=$(($1/$TIME))
         rm -f /data/test.img
         echo "Записано $1 MB. Запись ${SPEED_W} MB/s. Чтение ${SPEED_R} MB/s."
