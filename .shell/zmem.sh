956dbf7631c2b5909705291d99c2c5836351d345
Sergei Rozhkov <fgup@zserg.ru>
2025-01-09 21:57:46 +0500

    Manual Fix

--- .shell/zmem.sh
+++ .shell/zmem.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-/root/printer_data/scripts/ps_mem.py | sed 's/python3.7/Klipper/' | sed 's/python3.11/Moonraker/'
+/root/printer_data/scripts/ps_mem.py | sed 's/python3.7/Klipper/' | sed 's/python3.11/Moonraker/' | sed 's/firmwareExe/Экран/' | sed 's/mjpg_streamer/Камера/'
 free -m
 echo "Процессы в SWAP:"
-for file in /proc/*/status ; do awk '/VmSwap|Name/{printf $2 " " $3}END{ print ""}' $file; done |sort -k 2 -n -r|head -8| sed 's/python3.7/Klipper/' | sed 's/python3.11/Moonraker/'
-
+for file in /proc/*/status ; do awk '/VmSwap|Name/{printf $2 " " $3}END{ print ""}' $file; done |sort -k 2 -n -r|head -8| sed 's/python3.7/Klipper/' | sed 's/python3/Moonraker/' | sed 's/firmwareExe/Экран/' | sed 's/mjpg_streamer/Камера/'
--- .shell/zprint.sh
+++ .shell/zprint.sh
@@ -25,21 +25,23 @@ else
         then
             echo "EXCLUDE_OBJECT_DEFINE RESET=1" >/tmp/printer 2>/dev/null
             head -1000 "/data/$2" | grep ^EXCLUDE_OBJECT_DEFINE >/tmp/printer 2>/dev/null
+
+            M109=$(head -1000 "/data/$2" | grep "^M109" | head -1)
+            [ "$M109" == "" ] && M109=$(head -1000 "/data/$2" | grep "^M104" | head -1 | sed 's|M104|M109|')
+            M190=$(head -1000 "/data/$2" | grep "^M190" | head -1)
+            [ "$M190" == "" ] && M190=$(head -1000 "/data/$2" | grep "^M140" | head -1 | sed 's|M140|M190|')
+
+            if [ "$M190" == "" ] || [ "$M109" == "" ]
+                then
+                    echo "RESPOND TYPE=error MSG=\"В файле $2 не найдены команды нагрева стола(M140/M190) или сопла(M104/M109).\"" >/tmp/printer
+                    exit 1
+            fi
+
             if [ "$3" == "PRECLEAR" ]
                 then
-                    M109=$(head -1000 "/data/$2" | grep "^M109" | head -1)
-                    [ "$M109" == "" ] && M109=$(head -1000 "/data/$2" | grep "^M104" | head -1 | sed 's|M104|M109|')
-                    M190=$(head -1000 "/data/$2" | grep "^M190" | head -1)
-                    [ "$M190" == "" ] && M190=$(head -1000 "/data/$2" | grep "^M140" | head -1 | sed 's|M140|M190|')
-
-                    if [ "$M190" != "" ] && [ "$M109" != "" ]
-                        then
-                            echo "$M190" >/tmp/printer
-                            echo "$M109" >/tmp/printer
-                            echo "_START_PRECLEAR" >/tmp/printer
-                        else
-                            echo "RESPOND TYPE=error MSG=\"В файле $2 не найдены команды нагрева стола(M140/M190) или сопла(M104/M109).\"" >/tmp/printer
-                    fi
+                    echo "$M190" >/tmp/printer
+                    echo "$M109" >/tmp/printer
+                    echo "_START_PRECLEAR" >/tmp/printer
                     echo "RUN_SHELL_COMMAND CMD=zprint PARAMS=\"PRINT '$2'\"">/tmp/printer
                 else
                     $CURL -s \
