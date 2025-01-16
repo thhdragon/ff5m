327f8e084315cac9450f0fa1fe1d3eb2bab1b3f3
Alexander <DrA1ex@users.noreply.github.com>
2025-01-16 13:24:31 +0500

    Update starting script

--- .shell/root/start.sh
+++ .shell/root/start.sh
@@ -26,18 +26,33 @@ if [ "$SWAP" = "/root/swap" ]; then
     fi
 fi
 
+TIME_IN_SYNC=0
+
 synchronize_time() {
+    echo "Trying to synchronize time..."
     for server in ru.pool.ntp.org 1.ru.pool.ntp.org 2.ru.pool.ntp.org 3.ru.pool.ntp.org \
     4.ru.pool.ntp.org ntp1.vniiftri.ru ntp2.vniiftri.ru ntp3.vniiftri.ru \
     ntp4.vniiftri.ru ntp5.vniiftri.ru ntp.sstf.nsk.ru timesstf.sstf.nsk.ru ntp.kam.vniiftri.net; do
-        ntpd -dd -n -q -p $server && return
+        ntpd -dd -n -q -p $server > /dev/null 2>&1
+        if [ "$?" -eq 0 ]; then 
+            echo "Successfully synchronize time"
+            date
+            TIME_IN_SYNC=1
+            return
+        fi
     done
+    
+    echo "Unable to synchronize time"
 }
 
 if [ ! -f $NOT_FIRST_LAUNCH_F ]; then
+    echo "Initiali time synchronization"
+    
     date 2025.01.01-00:00:00
     synchronize_time
 fi
+
+echo "Starting services..."
 
 /opt/config/mod/.shell/root/S65moonraker start
 /opt/config/mod/.shell/root/S70httpd start
@@ -48,19 +63,21 @@ for _ in $(seq 0 30); do
     sleep 1
 done
 
+echo "Services started"
+
 echo "ZSSH_RELOAD" > /tmp/printer
 
+touch $NOT_FIRST_LAUNCH_F
 touch $START_PROC_DONE_F
 
-if [ ! -f $NOT_FIRST_LAUNCH_F ]; then
-    touch $NOT_FIRST_LAUNCH_F
-
+if [ "$TIME_IN_SYNC" -eq 0 ]; then
+    echo "Postponed time synchronization"
+    
     for _ in $(seq 0 50); do
         synchronize_time && break
         sleep 5
     done
     
     date
+    echo "Done"
 fi
-
-echo "Start END"
