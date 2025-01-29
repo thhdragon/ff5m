5727832259079044d3a9c3450da9973cf9106bd2
Alexander <DrA1ex@users.noreply.github.com>
2025-01-29 22:00:35 +0500

    Improve temperature_sensor gcode calling: - add throttle timeout - add option to reschedule event if i's called during the timeout

--- .py/klipper/patches/extras/temperature_sensor.py
+++ .py/klipper/patches/extras/temperature_sensor.py
@@ -27,7 +27,8 @@ class PrinterSensorGeneric:
                                         minval=KELVIN_TO_CELSIUS)
         self.max_temp = config.getfloat('max_temp', 99999999.9,
                                         above=self.min_temp)
-
+        self.gcode_throttle = config.getfloat("throttle", 1., minval=0)
+        self.gcode_reschedule = config.getboolean("reschedule", False)
         gcode_macro = self.printer.load_object(config, "gcode_macro")
         self.exceed_gcode_present = config.get("exceed_gcode", None) is not None
         if self.exceed_gcode_present:
@@ -39,6 +40,7 @@ class PrinterSensorGeneric:
         self.last_temp = 0.
         self.measured_min = 99999999.
         self.measured_max = 0.
+        self._last_exceed = 0
 
     m112_r = re.compile(r"^(?:[nN][0-9]+)?\s*[mM]112(?:\s|$)", re.MULTILINE)
 
@@ -49,21 +51,46 @@ class PrinterSensorGeneric:
             self.measured_max = max(self.measured_max, temp)
 
             if self.exceed_gcode_present and (temp < self.min_temp or temp > self.max_temp):
-                logging.info(f"[temperature_sensor {self.name}]: Out of range ({temp})")
+                self._handle_exceed(temp)
 
-                template = self.exceed_template.render()
-                # Run M112 immediately if present
-                if self.m112_r.search(template):
-                    self.printer.invoke_shutdown("Shutdown due to sensor value exceeding the limit")
-                    return
+    def _handle_exceed(self, temp):
+        logging.info(f"[temperature_sensor {self.name}]: Out of range ({temp})")
 
-                self.reactor.register_callback(lambda _: self._exceed_cb(template))
+        template = self.exceed_template.render()
+        # Run M112 immediately if present
+        if self.m112_r.search(template):
+            self.printer.invoke_shutdown("Shutdown due to sensor value exceeding the limit")
+            return
 
-    def _exceed_cb(self, template):
+        when = self.reactor.NOW
+        now = self.reactor.monotonic()
+        rescheduled = False
+        next_event = self._last_exceed + self.gcode_throttle
+        if next_event > now:
+            if not self.gcode_reschedule or next_event - now > self.gcode_throttle:
+                logging.info(f"[temperature_sensor {self.name}]: Exceed event skipped")
+                return
+
+            delta = next_event - now
+            when = now + delta
+            rescheduled = True
+            logging.info(f"[temperature_sensor {self.name}]: Reschedule exceed event after {delta:.2f}")
+
+        # Avoid repeated calls before the callback is executed
+        self._last_exceed = self.reactor.NEVER
+        self.reactor.register_callback(lambda _: self._exceed_cb(template, rescheduled), when)
+
+    def _exceed_cb(self, template, rescheduled):
+        # Re-render, as variables may change
+        if rescheduled:
+            template = self.gcode.run_script(self.exceed_template.render())
+
         try:
             self.gcode.run_script(template)
         except:
             logging.exception(f"[temperature_sensor {self.name}]: Script running error:\n{template}")
+
+        self._last_exceed = self.reactor.monotonic()
 
     def get_temp(self, eventtime):
         return self.last_temp, 0.
